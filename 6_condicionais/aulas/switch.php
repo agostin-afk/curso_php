bf9d0d3999b599f91b962d5bc0ebee69be2d4f64
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-04-16 18:45:43 -0300

    fim da seção 6

---
+++ 6_condicionais/aulas/switch.php
@@ -0,0 +1,15 @@
+<?php
+
+    $x = 45;
+    switch($x){
+        case 45:
+            echo "é o $x fi não tem jeito";
+            break;
+        case 0:
+            echo "não é o $x fi tem jeito";
+            break;
+        default:
+        echo "não";
+    }
+
+?>
