bb51d51e17dc2b5e7850939df681fe77e808c748
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-04-18 12:44:09 -0300

    fim da seção 7

---
+++ 7_repeticao/aulas/foreche.php
@@ -0,0 +1,12 @@
+<?php
+
+    $arr = ['agosto', 'pedro', 'lucas', 'manuel'];
+    
+    foreach($arr as $nome){
+        echo $nome . '<br>';
+        if ($nome == 'pedro'){
+            echo 'dale papai <br>';
+        }
+    }
+
+?>
