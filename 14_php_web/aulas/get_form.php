d31b49c82cbdeb28e622f6634e21c3843356440b
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-10-16 11:06:45 -0300

    formulário com get

---
+++ 14_php_web/aulas/get_form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="processamentos/processamento.php" method="get">
+        <div>
+            <input type="text" name="Nome" id="" placeholder="Digite seu nome">
+        </div>
+        <div>
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+</body>
+</html>
---
+++ 14_php_web/aulas/processamentos/processamento.php
@@ -0,0 +1,8 @@
+<?php
+
+    print_r($_GET);
+    $nome = $_GET['Nome'];
+
+?>
+
+<h1>Seu nome é: <?= $nome ?></h1>
